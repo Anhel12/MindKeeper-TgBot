346913a460ea81fc23233165eff01a1c292c1b5b
Anhel12 <gilevdmail@gmail.com>
2026-06-29 01:03:49 +0500

    init commit

---
+++ src/main/java/com/example/mindkeepertgbot/MindKeeperTgBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.mindkeepertgbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindKeeperTgBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindKeeperTgBotApplication.class, args);
+	}
+
+}
